827059f168b77ea2393d474d5d6718d2c1659c29
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-09-02 22:46:58 +0530

    video 27 : fetch API

--- src/components/News.js
+++ src/components/News.js
@@ -2,22 +2,40 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class News extends Component {
+  defaultImageUrl = 'https://images.hindustantimes.com/img/2022/09/02/1600x900/d6849b9e-2ab0-11ed-9b13-2e13773bcef4_1662118296534.jpg';
+  constructor() {
+    console.log('I am from constructor');
+    super();
+    this.state = {
+      articles: [],
+      loading :false
+    }
+  }
+
+  async componentDidMount(){
+    console.log('component did mount');
+    let url =  'https://newsapi.org/v2/top-headlines?country=in&apiKey=6dcdadbc546a431c83ff219a0e73afb8';
+    let data = await fetch(url);
+    let parsedData = await data.json(); 
+    console.log(parsedData);
+    this.setState({articles : parsedData.articles});
+  }
+
   render() {
+    console.log('I am from return');
     return (
       <div className="container">
         <h2>News Monkey - Top Headline</h2>
         <div className="row">
-          <div className="col-md-4">
-              <Newsitem title='my title' description = 'my description' />
-          </div><div className="col-md-4">
-              <Newsitem title='my title' description = 'my description' />
+        {this.state.articles.map((element) => {
+           return  <div className="col-md-4" key={element.url}>
+            <Newsitem
+              title={element.title?.slice(0,20)} description={element.description?.slice(0,40)} 
+              imageUrl={!element.urlToImage ? this.defaultImageUrl : element.urlToImage} newsUrl = {element.url}
+            />
           </div>
-          <div className="col-md-4">
-              <Newsitem title='my title' description = 'my description' />
-          </div>
-
+        })}          
         </div>
-        
       </div>
     );
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div className="card" style={{width: '18rem'}}>
-        <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="image"/>
+        <img src={imageUrl} className="card-img-top" alt="image"/>
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{title}</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
         </div>
       </div>
     )
