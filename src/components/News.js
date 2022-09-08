eeb19003bc50d184fe5500aa898cbfd8eaff5325
PROSUN619 <prosun.chakraborty92@gmail.com>
2022-09-08 08:57:04 -0500

    video 35: Add infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,134 +2,111 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types'
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 
-  static defaultProps={
-    country : 'in',
-    pageSize : 3,
-    category : 'general'
+  static defaultProps = {
+    country: 'in',
+    pageSize: 3,
+    category: 'general'
   }
 
   static propTypes = {
-    country : PropTypes.string,
-    pageSize : PropTypes.number,
-    category : PropTypes.string,
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
   }
 
   capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  //apiKey = '39c2a666d3ed4d2fba719c84a8ed178f'; //hotmail
+  //apiKey = '6dcdadbc546a431c83ff219a0e73afb8'; //gmail
 
   defaultImageUrl = 'https://images.hindustantimes.com/img/2022/09/02/1600x900/d6849b9e-2ab0-11ed-9b13-2e13773bcef4_1662118296534.jpg';
   constructor(props) {
-   // console.log('I am from constructor');
+    // console.log('I am from constructor');
     super(props);
     this.state = {
       articles: [],
       loading: true,
-      page: 1,
+      page: 0,
       totalResults: 0
     }
     document.title = `${this.capitalizeFirstLetter(props.category)} - News Monkey`
   }
 
   getData = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //debugger
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
-   // console.log(parsedData);
-    this.setState({ 
-      articles: parsedData.articles, 
+    // console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading:false 
+      loading: false,
+      page: this.state.page + 1
     });
   }
 
 
   async componentDidMount() {
     await this.getData();
-  //   //console.log('component did mount');
-  //   let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=1&pageSize=${this.props.pageSize}`;
-  //   let data = await fetch(url);
-  //   let parsedData = await data.json();
-  //  // console.log(parsedData);
-  //   this.setState({ 
-  //     articles: parsedData.articles, 
-  //     totalResults: parsedData.totalResults,
-  //     loading:false 
-  //   });
   }
 
-  handlePreviousClick = async () => {
-    this.setState({page: this.state.page - 1, loading: true});
-    await this.getData();
-    // //console.log('Handle previous click called');    
-    // this.setState({loading : true});
-    // let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // //console.log(parsedData);
-    // this.setState({
-    //   articles: parsedData.articles,
-    //   page: this.state.page - 1,
-    //   totalResults: parsedData.totalResults,
-    //   loading: false
-    // });
+  fetchMoreData = async () => {
+
+    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    //debugger
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+      page: this.state.page + 1
+    });
+
   }
 
-  handleNextClick = async () => {
-    this.setState({page: this.state.page + 1, loading: true});
-    await this.getData();
-    //console.log('Handle Next click called');
-    //debugger;
-    // if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-
-    // }
-    // else {
-    // }
-    // this.setState({loading : true});
-    // let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=6dcdadbc546a431c83ff219a0e73afb8&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // //console.log(parsedData);
-    // this.setState({
-    //   articles: parsedData.articles,
-    //   page: this.state.page + 1,
-    //   totalResults: parsedData.totalResults,
-    //   loading: false
-    // });
-  }
 
 
   render() {
-   // console.log('I am from return');
+
     return (
-      <div className="container">
+      <>
         <h2 className="text-center my-3">News Monkey - Top {this.capitalizeFirstLetter(this.props.category)} Headline</h2>
-        {this.state.loading && <Spinner/>}
-        
-          <div className="row">
-            { !this.state.loading && this.state.articles.map((element) => {
-              return <div className="col-md-4" key={element.url}>
-                <Newsitem
-                  title={element.title?.slice(0, 40)} description={element.description?.slice(0, 80)}
-                  imageUrl={!element.urlToImage ? this.defaultImageUrl : element.urlToImage} newsUrl={element.url}
-                  author={element.author} date={new Date(element.publishedAt).toGMTString()} sourceName={element.source.name}
-                />
-              </div>
-            })}
-            
-            <div className="container d-flex justify-content-between my-3">
-              <button type="button" disabled={this.state.page <= 1} className="btn btn-danger" onClick={this.handlePreviousClick}>&laquo; Previous</button>
-              <p><strong>Page No {this.state.page}/{Math.ceil(this.state.totalResults / this.props.pageSize)}</strong></p>
-              <button type="button" disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))} className="btn btn-danger" onClick={this.handleNextClick}>Next &raquo;</button>
+        {/* {this.state.loading && <Spinner/>} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.totalResults}
+          loader={this.state.loading && <Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {
+                this.state.articles.map((element) => {
+                  return <div className="col-md-4 my-2" key={element.url}>
+                    <Newsitem
+                      title={element.title?.slice(0, 40)} description={element.description?.slice(0, 80)}
+                      imageUrl={!element.urlToImage ? this.defaultImageUrl : element.urlToImage} newsUrl={element.url}
+                      author={element.author} date={new Date(element.publishedAt).toGMTString()} sourceName={element.source.name}
+                    />
+                  </div>
+                })
+              }
             </div>
           </div>
-      </div>
-        
+        </InfiniteScroll>
+      </>
+
     );
   }
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Spinner extends Component {
   render() {
     return (
-      <div className='text-center'>
+      <div className='text-center my-3'>
         <img src='Spinner.gif' alt='Loading...'  />
       </div>
     )
